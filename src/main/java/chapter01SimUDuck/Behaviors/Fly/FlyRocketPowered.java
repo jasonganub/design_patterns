126a9195bc1dd912ff39da5b0b01a7c82743b695
Jason Ganub <jasonganub@gmail.com>
2018-08-26 14:07:49 +0700

    Create FlyRocketPowered Behavior and set model duck on runtime to use it.

---
+++ src/main/java/chapter01SimUDuck/Behaviors/Fly/FlyRocketPowered.java
@@ -0,0 +1,8 @@
+package chapter01SimUDuck.Behaviors.Fly;
+
+public class FlyRocketPowered implements FlyBehavior {
+
+    public void fly() {
+        System.out.println("I'm flying with a rocket");
+    }
+}
--- src/main/java/chapter01SimUDuck/MiniDuckSimulator.java
+++ src/main/java/chapter01SimUDuck/MiniDuckSimulator.java
@@ -1,12 +1,19 @@
 package chapter01SimUDuck;
 
+import chapter01SimUDuck.Behaviors.Fly.FlyBehavior;
+import chapter01SimUDuck.Behaviors.Fly.FlyRocketPowered;
 import chapter01SimUDuck.Ducks.Duck;
 import chapter01SimUDuck.Ducks.MallardDuck;
+import chapter01SimUDuck.Ducks.ModelDuck;
 
 public class MiniDuckSimulator {
     public static void main(String[] args) {
         Duck mallard = new MallardDuck();
         mallard.performFly();
         mallard.performQuack();
+
+        Duck model = new ModelDuck();
+        model.setFlyBehavior(new FlyRocketPowered());
+        model.performFly();
     }
 }
